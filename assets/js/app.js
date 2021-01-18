360369c82efaa67a3cba3fe01f7534f56116fb52
mathiasone <m.ferreyra1985@gmail.com>
2021-01-18 12:05:03 -0300

    mejoras para entrega final 18/01_v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ window.addEventListener('load', function(){
 		responsive: [
 			{
 			  // screens greater than >= 775px
-			  breakpoint: 450,
+			  breakpoint: 650,
 			  settings: {
 				// Set to `auto` and provide item width to adjust to viewport
 				slidesToShow: 2,
@@ -20,10 +20,24 @@ window.addEventListener('load', function(){
 			  // screens greater than >= 1024px
 			  breakpoint: 800,
 			  settings: {
-				slidesToShow: 4,
-				slidesToScroll: 4
+				slidesToShow: 3,
+				slidesToScroll: 3
 			  }
-			}
+			},{
+				// screens greater than >= 1024px
+				breakpoint: 1024,
+				settings: {
+				  slidesToShow: 4,
+				  slidesToScroll: 4
+				}
+			},{
+				// screens greater than >= 1024px
+				breakpoint: 1279,
+				settings: {
+				  slidesToShow: 5,
+				  slidesToScroll: 5
+				}
+			  }
 		]
 	});
 });
