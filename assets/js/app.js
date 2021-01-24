f3d2b7fbfe91ce9d9a82b59995c052ce1fee7a0e
mathiasone <m.ferreyra1985@gmail.com>
2021-01-24 14:12:40 -0300

    correciones de detalles para entrega final

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,28 +10,25 @@ window.addEventListener('load', function(){
 		responsive: [
 			{
 			  // screens greater than >= 775px
-			  breakpoint: 650,
+			  breakpoint: 400,
 			  settings: {
 				// Set to `auto` and provide item width to adjust to viewport
 				slidesToShow: 2,
 				slidesToScroll: 2
 			  }
 			},{
-			  // screens greater than >= 1024px
 			  breakpoint: 769,
 			  settings: {
 				slidesToShow: 3,
 				slidesToScroll: 3
 			  }
 			},{
-				// screens greater than >= 1024px
 				breakpoint: 1024,
 				settings: {
 				  slidesToShow: 4,
 				  slidesToScroll: 4
 				}
 			},{
-				// screens greater than >= 1024px
 				breakpoint: 1279,
 				settings: {
 				  slidesToShow: 5,
